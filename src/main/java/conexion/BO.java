024469215c5c7db390fcf567cc438992339a578d
chuvacagapj@hotmail.com <Jesus Garcia>
2015-04-18 16:17:31 -0600

    se define el esqueleto de BO

--- src/main/java/conexion/BO.java
+++ src/main/java/conexion/BO.java
@@ -5,6 +5,9 @@
  */
 package conexion;
 
+import vo.*;
+import java.util.ArrayList;
+
 /**
  * La funcion de esta clase es la de hacer de interfaz entre los DAO y el codigo
  * cliente, debe ser capas de devolver los objeto VO correspondientes, manejar 
@@ -21,4 +24,148 @@ public class BO {
     /* TODO faltan los atributos, debe haber un atributo por cada objeto DAO
             que exita en el paquete dao
     */
+    
+    // funciones create
+    
+    public void createCliente(Cliente cliente){
+        //TODO
+    }
+    
+    public void createCorrida(Corrida corrida){
+        //TODO
+    }
+    
+    public void createEmpresa(Empresa cliente){
+        //TODO
+    }
+    
+    public void createEstado(Estado estado){
+        //TODO
+    }
+    
+    public void createLocacion(Locacion locacion){
+        //TODO
+    }
+    
+    public void createMunicipio(Municipio municipio){
+        //TODO
+    }
+    
+    public void createReservacion(Reservacion reservacion){
+        //TODO
+    }
+    
+    public void createTransporte(Transporte transporte){
+        //TODO
+    }
+    
+    // funciones read
+    
+    public ArrayList <Cliente> readCliente(Cliente cliente){
+        //TODO
+        return null;
+    }
+    
+    public ArrayList <Corrida> readCorrida(Corrida corrida){
+        //TODO
+        return null;
+    }
+    
+    public ArrayList <Empresa> readEmpresa(Empresa cliente){
+        //TODO
+        return null;
+    }
+    
+    public ArrayList <Estado> readEstado(Estado estado){
+        //TODO
+        return null;
+    }
+    
+    public ArrayList <Locacion> readLocacion(Locacion locacion){
+        //TODO
+        return null;
+    }
+    
+    public ArrayList <Municipio> readMunicipio(Municipio municipio){
+        //TODO
+        return null;
+    }
+    
+    public ArrayList <Reservacion> readReservacion(Reservacion reservacion){
+        //TODO
+        return null;
+    }
+    
+    public ArrayList <Transporte> readTransporte(Transporte transporte){
+        //TODO
+        return null;
+    }
+    
+    // funciones update
+    
+    public void updateCliente(Cliente cliente){
+        //TODO
+    }
+    
+    public void updateCorrida(Corrida corrida){
+        //TODO
+    }
+    
+    public void updateEmpresa(Empresa cliente){
+        //TODO
+    }
+    
+    public void updateEstado(Estado estado){
+        //TODO
+    }
+    
+    public void updateLocacion(Locacion locacion){
+        //TODO
+    }
+    
+    public void updateMunicipio(Municipio municipio){
+        //TODO
+    }
+    
+    public void updateReservacion(Reservacion reservacion){
+        //TODO
+    }
+    
+    public void updateTransporte(Transporte transporte){
+        //TODO
+    }
+    
+    // funciones delete
+    
+    public void deleteCliente(Cliente cliente){
+        //TODO
+    }
+    
+    public void deleteCorrida(Corrida corrida){
+        //TODO
+    }
+    
+    public void deleteEmpresa(Empresa cliente){
+        //TODO
+    }
+    
+    public void deleteEstado(Estado estado){
+        //TODO
+    }
+    
+    public void deleteLocacion(Locacion locacion){
+        //TODO
+    }
+    
+    public void deleteMunicipio(Municipio municipio){
+        //TODO
+    }
+    
+    public void deleteReservacion(Reservacion reservacion){
+        //TODO
+    }
+    
+    public void deleteTransporte(Transporte transporte){
+        //TODO
+    }
 }
